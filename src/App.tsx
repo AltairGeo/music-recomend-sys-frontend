15f2b083c543cd58540d691ccf38351ac0ed63ba
AltairGeo <q>
2026-04-25 16:56:21 +0300

    Init base frontend with mantine

---
+++ src/App.tsx
@@ -0,0 +1,32 @@
+import { AppShell, Burger, Group, Text } from "@mantine/core";
+import { useDisclosure } from "@mantine/hooks";
+function App() {
+  const [opened, { toggle }] = useDisclosure();
+
+  return (
+    <>
+      <AppShell
+        padding="md"
+        header={{ height: 60 }}
+        navbar={{
+          width: 300,
+          breakpoint: "sm",
+          collapsed: { mobile: !opened },
+        }}
+      >
+        <AppShell.Header>
+          <Group h="100%" px="md">
+            <Burger opened={opened} onClick={toggle} hiddenFrom="sm" />
+            <Text fw={700}>MusProject</Text>
+          </Group>
+        </AppShell.Header>
+
+        <AppShell.Navbar p="md">Sidebar</AppShell.Navbar>
+
+        <AppShell.Main>Content</AppShell.Main>
+      </AppShell>
+    </>
+  );
+}
+
+export default App;
